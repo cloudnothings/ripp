82214f79b63593e6c55da0ea2c157832f1e615c5
Carlos Rangel <carlos@thinkrelion.com>
2022-08-11 07:31:30 -0700

    tets

--- src/store/api/getTicketList.js
+++ src/store/api/getTicketList.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 const getTicketList = async (filter) => {
   const getTicketListResult = await axios({
     method: 'post',
-    baseURL: window.location.origin,
     url: '/api/GetTicketList',
     headers: {
       'Content-Type': 'application/json',
